53b8184f2cc36a43354fad558960ea4e8e8a857c
BdFreitas <brenodfreitas7@gmail.com>
2022-07-08 23:33:40 -0300

    removing back

--- java-api/src/main/java/series/javaapi/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package series.javaapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import series.javaapi.entity.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Integer>
-{
-}
